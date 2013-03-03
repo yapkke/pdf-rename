4ca7c6ae8670f44e08b94edfe5c5bb1afc425564
KK <ykk@kk-alien>
2013-03-02 23:23:03 -0800

    Allows moddate to be change for the year

--- bin/pdf-author-title.sh
+++ bin/pdf-author-title.sh
@@ -2,11 +2,30 @@
 if [ -z "$3" ]
 then
    # Error message to stderr.
-   echo "Usage: `basename $0` <input file> <author> <title>" >&2
+   echo "Usage: `basename $0` <input file> <author> <title> [year]" >&2
    # Returns 65 as exit status of script (error code).
    exit $NOARGS
-fi  
+fi
+  
+if [ -z "$4" ]
+then
+    echo "Year is not supplied." 
+else
+    pdf-info-change.py "$1" ModDate "D:${4}01010000-00" 
+fi
 
-pdf-info-change.py "$1" Author "$2"
-pdf-info-change.py "$1" Title "$3"
+if [ -z "$3" ]
+then
+    echo "Title is not supplied." 
+else
+    pdf-info-change.py "$1" Title "$3"
+fi
+
+if [ -z "$2" ]
+then
+    echo "Author is not supplied." 
+else
+    pdf-info-change.py "$1" Author "$2"
+fi
+
 pdfinfo "$1"
